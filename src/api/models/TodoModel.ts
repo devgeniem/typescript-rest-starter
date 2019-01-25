1758f70c8a1d3f817736196aab41597047ab1c9f
Mika Puhakka <mika.puhakka@geniem.com>
2019-01-25 17:57:35 +0200

    stuff

--- src/api/models/TodoModel.ts
+++ src/api/models/TodoModel.ts
@@ -4,6 +4,11 @@ import { addSchema } from 'objection-ts-schema';
 export class TodoModel extends Model {
   static tableName = 'todo';
 
-  @addSchema id: number;
-  @addSchema text: string;
+  @addSchema() id: number;
+  @addSchema(true) text: string;
+
+  $beforeValidate(schema, json, opt) {
+    console.log('schema', schema)
+    return super.$beforeValidate(schema, json, opt)
+  }
 }
